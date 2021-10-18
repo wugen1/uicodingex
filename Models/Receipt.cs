7ce1a64dd8e65eca8ffc61eedef1b1d0dd9d8e25
unknown <r2cutting@gmail.com>
2021-10-18 15:17:39 -0500

    First commit

---
+++ Models/Receipt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingEx.Models
+{
+    public class Receipt
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name")]
+        [Display(Name = "Employee Name")]
+        [StringLength(50)] 
+        public string EmployeeName { get; set; }
+
+        [Required(ErrorMessage = "write brief description for your receipt")]
+        [StringLength(50)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PurchaseDate { get; set; }
+        
+        [Required(ErrorMessage = "Upload your receipt image here")]
+        public string ReceiptImg { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
---
+++ ViewModels/ReceiptViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingEx.ViewModels
+{
+    public class ReceiptViewModel
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        [Display(Name = "Employee Name")]
+        public string EmployeeName { get; set; }
+
+        [Required(ErrorMessage = "write brief description for your receipt")]
+        [StringLength(50)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PurchaseDate { get; set; }
+
+        [Required(ErrorMessage = "Upload your receipt image here")]
+        [Display(Name = "Receit File")]
+        public IFormFile ReceiptImg { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
